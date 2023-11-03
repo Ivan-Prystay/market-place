07c0f69ac42061bd75efdda6e77a348a172a4d0e
Ivan Prystay <prystay.ivan@gmail.com>
2023-11-03 23:52:25 +0200

    546

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: { unoptimized: true },
+  experimental: { images: { unoptimized: true } },
+  basePath: "/market-place",
   output: "export",
 };
 
