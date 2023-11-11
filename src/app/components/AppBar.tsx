b9cfdc86e88eb330053a9989f9d82d77eaaff2b8
Ivan Prystay <prystay.ivan@gmail.com>
2023-11-11 19:11:47 +0200

    add Suspense

--- src/app/components/AppBar.tsx
+++ src/app/components/AppBar.tsx
@@ -1,12 +1,18 @@
+import { Suspense } from "react";
 import Logo from "./Logo";
 import NavBar from "./NavBar";
+
 import SigninButton from "./SigninButton";
 
 const AppBar = () => {
   return (
     <header className="flex items-center justify-between h-40 border-b border-white/30 px-20">
       <Logo />
-      <NavBar />
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <NavBar />
+      </Suspense>
+
       <SigninButton />
     </header>
   );
